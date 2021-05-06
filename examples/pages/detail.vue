a9432eecc94adfa5a1cc411217aa348b4e53db4f
hezedu <tianxiazhizhong@gmail.com>
2021-05-06 22:08:47 +0800

    examples sync

--- examples/pages/detail.vue
+++ examples/pages/detail.vue
@@ -1,7 +1,7 @@
 <template>
 <div style="height: 3000px; padding: 10px;">
   <h1>Detail</h1>
-  <button @click="back">back3</button>
+  <button @click="back">back to start {{$page.stateKey}}</button>
   <br>
   <navigator to="/" relaunch>Relaunch to Index</navigator>
 </div>
