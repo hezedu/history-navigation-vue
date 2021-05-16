028ed3528bdd1f98c8f94e53d87ea78ec0ab4680
hezedu <tianxiazhizhong@gmail.com>
2021-05-16 10:32:13 +0800

    transition.scss split ok

---
+++ src/cmpt/tab-bar-controller.vue
@@ -0,0 +1,80 @@
+<template>
+<transition name="h-nav-page" :appear="true">
+  <div class="h-nav-tabs-ctrler h-nav-tabs-wrap" v-show="isActive" :style="{zIndex: currTabPage.stateKey}">
+    <transition-group 
+      class="h-nav-tab-page-wrap" 
+      tag="div" 
+      :class="tabBehavior" 
+      enter-class=""
+      leave-class=""
+      enter-to-class=""
+      leave-to-class=""
+      enter-active-class="h-nav-tab_page_load"
+      leave-active-class="h-nav-tab_page_unload">
+
+      <div class="h-nav-tab-page-container" 
+          v-for="v in tabStackMap"
+          :class="v.className"
+          :key="v.tabIndex">
+          <Page
+            transitionName="h-nav-tab-page"
+            :path="v.path"
+            :title="v.title"
+            
+            :isTab="v.isTab"
+            :tabIndex="v.tabIndex"
+            :stateKey="v.stateKey"
+            :route="v.route"
+            :style="{zIndex: v.tabIndex}"
+            :isActive="isActive && (currTabPage.tabIndex === v.tabIndex)"
+            :isFirstLoad="isFirstLoad">
+            <component :is="v.cmptKey" />
+          </Page>
+      </div>
+    </transition-group>
+
+    <TabBar :list="tabList" :currentIndex="currTabPage.tabIndex" :style="{zIndex: tabList.length}" />
+  </div>
+</transition>
+</template>
+<script>
+import Page from './page.vue';
+import TabBar from './tab-bar.vue';
+
+export default {
+  name: "HistoryNavigationTabBarController",
+  components: {
+    Page,
+    TabBar
+  },
+  props: {
+    currTabPage: {
+      type: Object,
+      required: true
+    },
+    isActive: {
+      type: Boolean,
+      required: true
+    },
+    isFirstLoad: {
+      type: Boolean,
+      required: true
+    }
+  },
+  
+  data(){
+    return {
+      tabStackMap: this.$navigator._h.tabStackMap,
+      tabList: this.$navigator._h.tabList,
+      tabBehavior: ''
+    }
+  },
+  watch: {
+    'currTabPage.tabIndex'(newVal, oldVal){
+      let type = newVal > oldVal ? 'greater' : 'smaller';
+
+      this.tabBehavior = 'h-nav-tab-switch-' + type;
+    }
+  }
+}
+</script>
