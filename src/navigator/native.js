cebb8ccc431029c2d1e3c32c792c3ea549ecf76d
hezedu <tianxiazhizhong@gmail.com>
2021-05-19 12:40:46 +0800

    add  transition-view.vue

--- src/navigator/native.js
+++ src/navigator/native.js
@@ -1,11 +1,15 @@
 
 
-let nativeWindow, nativeHistory, nativeLocation;
+let nativeWindow, nativeHistory, nativeLocation, nativeRAF;
 
 if(typeof window !== 'undefined'){
   nativeWindow = window;
   nativeHistory = window.history;
   nativeLocation = window.location;
+  nativeRAF = window.requestAnimationFrame || setTimeout;
+} else {
+  // nativeRAF = function(cb){cb()};
+  nativeRAF = setTimeout;
 }
 
-export {nativeWindow, nativeHistory, nativeLocation}
+export {nativeWindow, nativeHistory, nativeLocation, nativeRAF}
